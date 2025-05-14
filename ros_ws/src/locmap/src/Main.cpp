f959b9642093197566ac292e0c74ba09995a4fa6
james <james2004.coward@gmail.com>
2025-05-14 02:03:39 +0100

    Reformatted locmap & added locmap params

--- ros_ws/src/locmap/src/Main.cpp
+++ ros_ws/src/locmap/src/Main.cpp
@@ -2,15 +2,15 @@
 // Created by jamestbest on 3/27/25.
 //
 
+/**
+ * locmap is an abstraction over the map providing locations with names instead of pose data
+ *
+ * locmap has two modes
+ *  - Record: this is for giving names to goal locations
+ *  - Publish: this is for publishing the known locations and listening for goto requests
+ */
+
 #include "Main.h"
-
-#include "ros/ros.h"
-#include "geometry_msgs/PoseStamped.h"
-#include "actionlib_msgs/GoalID.h"
-
-#include "locmap/LocMapLocations.h"
-#include "locmap/LocMapLocation.h"
-#include "locmap/LocMapGoto.h"
 
 #include <fstream>
 #include <string>
@@ -35,8 +35,222 @@ bool record_mode= false;
 // this is just a default value for if not using the roslaunch, if that's the case then use rosrun in the same directory
 string location_data_dir= "./location_data/";
 
+// ros params
+string  LOCATIONS_TOPIC,
+        GOAL_TOPIC,
+        GOTO_TOPIC,
+        CANCEL_TOPIC;
+
 locmap::LocMapLocations locations;
 
+/**
+ * Simple function to initialise the ros node, must be called before any ros calls
+ */
+static void init_ros(int argc, char** argv) {
+    ros::init(argc, argv, "talker");
+}
+
+/**
+ * Ros contains the parameters from the param.yaml file
+ *  these are shared across the different nodes to allow easy changes
+ * @param node The initialised ros NodeHandle
+ */
+static void setup_params(const ros::NodeHandle& node) {
+    node.getParam("locations_dir"  , location_data_dir);
+    node.getParam("LOCATIONS_TOPIC", LOCATIONS_TOPIC);
+    node.getParam("GOTO_TOPIC"     , GOTO_TOPIC);
+    node.getParam("GOAL_TOPIC"     , GOAL_TOPIC);
+    node.getParam("END_GOAL_TOPIC" , CANCEL_TOPIC);
+}
+
+/**
+ * Entry point function
+ *  Will setup the ros node with its pubs and subs
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char** argv) {
+    string res;
+
+    if (argc > 1 && (strcmp(argv[1], "pub") == 0)) {
+        record_mode= false;
+        goto main_init;
+    }
+
+    cout << "Record? (y/n): ";
+    getline(cin, res);
+
+    if (res == "y") {
+        record_mode= true;
+    }
+
+    main_init:
+    init_ros(argc, argv);
+
+    const ros::NodeHandle node;
+
+    setup_params(node);
+
+    cout << "DIR " << location_data_dir << endl;
+
+    create_subscribers(node);
+    create_publishers(node);
+
+    if (deserialize_all_files()) {
+        publish_locations();
+    }
+
+    cout << locations << endl;
+
+    ros::spin();
+
+    return EXIT_SUCCESS;
+}
+
+/**
+ * Create the two subscribers needed namely
+ *  the goal subscriber which is used in record mode
+ *  the goto subscriber which is used in publisher mode (for goto requests)
+ * @param node
+ */
+static void create_subscribers(ros::NodeHandle node) {
+    cout << "Creating listeners" << endl;
+    goal_subscriber= node.subscribe(GOAL_TOPIC, 10, goal_callback);
+
+    goto_subscriber= node.subscribe(GOTO_TOPIC, 1, goto_callback);
+}
+
+/**
+ * Create the three publishers needed namely
+ *  the location publisher which is used to publish all locmap known locations
+ *  the end goal publisher which is used in record mode to cancel the goal when a user marks a location with a name
+ *  the goal publisher which is used to send the pose information for the goto request to move_base
+ * @param node
+ */
+static void create_publishers(ros::NodeHandle node) {
+    cout << "Creating publishers" << endl;
+    location_publisher= node.advertise<locmap::LocMapLocations>(LOCATIONS_TOPIC, 1, true);
+    end_goal_publisher= node.advertise<actionlib_msgs::GoalID>(CANCEL_TOPIC, 1, false);
+
+    goal_publisher= node.advertise<geometry_msgs::PoseStamped>(GOAL_TOPIC, 1, false);
+}
+
+/**
+ * Called when the goto subscriber receives a request to go to a location
+ *  the goal location just contains a location name which is checked against known locations
+ * @param goal_location
+ */
+static void goto_callback(locmap::LocMapGoto goal_location) {
+    locmap::LocMapLocation location;
+
+    if (!find_location(goal_location.location_name.c_str(), &location)) {
+        ROS_WARN("Unable to find requested location `%s`", goal_location.location_name.c_str());
+        return;
+    }
+
+    if (record_mode) {
+        return;
+    }
+
+    goal_publisher.publish(location.pose);
+}
+
+/**
+ * Called when the goal subscriber receives a new goal
+ *  this is only used in record mode
+ * @param goal
+ */
+static void goal_callback(geometry_msgs::PoseStamped goal) {
+    if (!record_mode) {
+        cout << "Ignored non-record mode goal" << endl;
+        return;
+    }
+
+    cout << "Got new goal" << endl;
+    actionlib_msgs::GoalID id= actionlib_msgs::GoalID();
+    cout << "Publishing cancel goal" << endl;
+    end_goal_publisher.publish(id);
+
+    string loc_name;
+    cout << "Enter location name: " << endl;
+    getline(cin, loc_name);
+    cout << "Location designated as: " << loc_name << endl;
+
+    print_cwd();
+
+    locmap::LocMapLocation location;
+    location.pose= goal;
+    location.name= loc_name;
+    location.header= goal.header;
+    location.group= "None";
+
+    serialize_location(location);
+    add_location(location);
+}
+
+/**
+ * publish all known locations
+ *  only called at the start or when a new location is added
+ */
+static void publish_locations() {
+    location_publisher.publish(locations);
+}
+
+/**
+ * Find from the known locations the location that matches the name
+ * @param name name of location to find
+ * @param loc_out pointer to the LocMapLocation struct that will be set if found
+ * @return if found location
+ */
+static bool find_location(string name, locmap::LocMapLocation* loc_out) {
+    for (auto& loc: locations.locations) {
+        if (loc.name == name) {
+            *loc_out= loc;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Add a new location to the known locations list
+ *  update the published locations
+ * @param location
+ */
+static void add_location(locmap::LocMapLocation location) {
+    locations.locations.push_back(location);
+
+    publish_locations();
+}
+
+/**
+ * Locations include a group tag
+ * @param group name of group
+ * @return All locations within the group
+ */
+static locmap::LocMapLocations get_all_in_group(string group) {
+    locmap::LocMapLocations location_ret;
+
+    location_ret.groups= {group};
+
+    location_ret.locations= vector<locmap::LocMapLocation>(locations.locations.size());
+
+    for (const auto& loc: locations.locations) {
+        if (loc.group == group)
+            location_ret.locations.push_back(loc);
+    }
+
+    return location_ret;
+}
+
+/**
+ * Open a file for writing binary data
+ * @param filename
+ * @param ofs output file stream
+ * @return
+ */
 static bool open_serialization_file(string filename, ofstream& ofs) {
     ofs.open(filename, ios::binary);
 
@@ -48,6 +262,13 @@ static bool open_serialization_file(string filename, ofstream& ofs) {
     return true;
 }
 
+/**
+ * Open a file for reading binary data
+ * @param file_path
+ * @param ifs input file stream
+ * @param size_out size of the file
+ * @return
+ */
 static bool open_deserialization_file(string file_path, ifstream& ifs, streamsize* size_out) {
     ifs.open(file_path, ios::binary | ios::ate);
 
@@ -62,6 +283,12 @@ static bool open_deserialization_file(string file_path, ifstream& ifs, streamsiz
     return true;
 }
 
+/**
+ * Deserialize the binary file that contains a location
+ * @param file_path
+ * @param location reference to the output location struct
+ * @return
+ */
 static bool deserialize_file(string file_path, locmap::LocMapLocation& location) {
     ifstream ifs;
 
@@ -83,11 +310,13 @@ static bool deserialize_file(string file_path, locmap::LocMapLocation& location)
     return true;
 }
 
+/**
+ * Deserialize all location files within the location data directory
+ * @return
+ */
 static bool deserialize_all_files() {
     namespace efs= std::experimental::filesystem;
     vector<locmap::LocMapLocation> loc_vec;
-
-    // todo get the number of entries for the size of the vector
 
     for (const auto& file: efs::directory_iterator(location_data_dir)) {
         cout << "Deserializing " << file.path() << endl;
@@ -108,6 +337,11 @@ static bool deserialize_all_files() {
     return true;
 }
 
+/**
+ * Write a location to the file system
+ * @param location
+ * @return
+ */
 static bool serialize_location(const locmap::LocMapLocation location) {
     ofstream ofs;
     const string file_path= location_data_dir + location.name;
@@ -130,6 +364,9 @@ static bool serialize_location(const locmap::LocMapLocation location) {
     return true;
 }
 
+/**
+ * Debugging; shows the current working directory
+ */
 static void print_cwd() {
     char cwd[PATH_MAX];
     if (getcwd(cwd, sizeof(cwd)) != nullptr) {
@@ -137,150 +374,4 @@ static void print_cwd() {
     } else {
         perror("getcwd() error");
     }
-}
-
-static void goal_callback(geometry_msgs::PoseStamped goal) {
-    if (!record_mode) {
-        cout << "Ignored non-record mode goal" << endl;
-        return;
-    }
-
-    cout << "Got new goal" << endl;
-
-    actionlib_msgs::GoalID id= actionlib_msgs::GoalID();
-
-    cout << "Publishing cancel goal" << endl;
-    end_goal_publisher.publish(id);
-
-    string loc_name;
-    cout << "Enter location name: " << endl;
-    getline(cin, loc_name);
-    cout << "Location designated as: " << loc_name << endl;
-
-    print_cwd();
-
-    locmap::LocMapLocation location;
-    location.pose= goal;
-    location.name= loc_name;
-    location.header= goal.header;
-    location.group= "None";
-
-    serialize_location(location);
-    add_location(location);
-}
-
-static bool find_location(string name, locmap::LocMapLocation* loc_out) {
-    cout << "Finding " << name << endl;
-    for (auto& loc: locations.locations) {
-        cout << "Checking against `" << loc.name << "`" << endl;
-        if (loc.name == name) {
-            *loc_out= loc;
-            return true;
-        }
-    }
-
-    return false;
-}
-
-static void goto_callback(locmap::LocMapGoto goal_location) {
-    locmap::LocMapLocation location;
-
-    if (!find_location(goal_location.location_name.c_str(), &location)) {
-        ROS_WARN("Unable to find requested location `%s`", goal_location.location_name.c_str());
-        return;
-    }
-
-    cout << "Found location: " << goal_location.location_name << endl;
-
-    if (record_mode) {
-        return;
-    }
-
-    goal_publisher.publish(location.pose);
-}
-
-static void create_subscribers(ros::NodeHandle node) {
-    cout << "Creating listeners" << endl;
-    goal_subscriber= node.subscribe("/move_base_simple/goal", 10, goal_callback);
-
-    goto_subscriber= node.subscribe("/locmap/goto", 1, goto_callback);
-}
-
-static void create_publishers(ros::NodeHandle node) {
-    cout << "Creating publishers" << endl;
-    location_publisher= node.advertise<locmap::LocMapLocations>("/locmap/locations", 1, true);
-    end_goal_publisher= node.advertise<actionlib_msgs::GoalID>("/move_base/cancel", 1, false);
-
-    goal_publisher= node.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1, false);
-}
-
-static void publish_locations() {
-    location_publisher.publish(locations);
-}
-
-static void add_location(locmap::LocMapLocation location) {
-    locations.locations.push_back(location);
-
-    publish_locations();
-}
-
-static locmap::LocMapLocations get_all_in_group(string group) {
-    locmap::LocMapLocations location_ret;
-
-    location_ret.groups= {group};
-
-    location_ret.locations= vector<locmap::LocMapLocation>(locations.locations.size());
-
-    for (const auto& loc: locations.locations) {
-        if (loc.group == group)
-            location_ret.locations.push_back(loc);
-    }
-
-    return location_ret;
-}
-
-static void setup_params(ros::NodeHandle node) {
-    node.getParam("locations_dir", location_data_dir);
-}
-
-static void init_ros(int argc, char** argv) {
-    ros::init(argc, argv, "talker");
-}
-
-int main(int argc, char** argv) {
-    string res;
-
-    if ((strcmp(argv[1], "pub") == 0)) {
-        record_mode= false;
-        goto main_init;
-    }
-
-    cout << "Record? (y/n): ";
-    getline(cin, res);
-
-    if (res == "y") {
-        record_mode= true;
-    }
-
-main_init:
-    init_ros(argc, argv);
-
-    const ros::NodeHandle node;
-
-    setup_params(node);
-
-    cout << "DIR " << location_data_dir << endl;
-
-    create_subscribers(node);
-    create_publishers(node);
-
-    if (deserialize_all_files()) {
-        publish_locations();
-    }
-
-    cout << locations << endl;
-
-    ros::spin();
-
-    return EXIT_SUCCESS;
 }
--- ros_ws/src/locmap/src/Main.h
+++ ros_ws/src/locmap/src/Main.h
@@ -5,10 +5,27 @@
 #ifndef CATKIN_WS_MAIN_H
 #define CATKIN_WS_MAIN_H
 
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "actionlib_msgs/GoalID.h"
 
-class Main {
+#include "locmap/LocMapLocations.h"
+#include "locmap/LocMapLocation.h"
+#include "locmap/LocMapGoto.h"
 
-};
+static void create_subscribers(ros::NodeHandle node);
+static void create_publishers(ros::NodeHandle node);
 
+static void goal_callback(geometry_msgs::PoseStamped goal);
+static void goto_callback(locmap::LocMapGoto goal_location);
+
+static bool deserialize_all_files();
+static bool serialize_location(const locmap::LocMapLocation location);
+
+static void publish_locations();
+
+static bool find_location(std::string name, locmap::LocMapLocation* loc_out);
+
+static void print_cwd();
 
 #endif //CATKIN_WS_MAIN_H
